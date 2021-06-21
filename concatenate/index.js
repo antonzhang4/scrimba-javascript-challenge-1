298d02e6a4531dfe086f6094632d29bd6ceaf3dc
antonzhang4 <58164836+antonzhang4@users.noreply.github.com>
2021-06-22 09:48:09 +1000

    completed javascript 1 challenges

---
+++ concatenate/index.js
@@ -0,0 +1,7 @@
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+function myLogger() {
+    console.log(greeting + ", " + name + "!")
+}
